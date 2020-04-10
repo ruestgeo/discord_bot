be14c0d057f27ca8a8449849706ece134ae1858a
Geoffrey J P DelaVega Ruest <geoffrey.ruest@mail.utoronto.ca>
2020-04-10 10:54:04 -0400

    removed modules, added configs and examples

--- bot.js
+++ bot.js
@@ -1,10 +1,22 @@
+/*
+GNU General Public License v3.0
+
+Permissions of this strong copyleft license are conditioned on making available 
+complete source code of licensed works and modifications, which include larger 
+works using a licensed work, under the same license. Copyright and license 
+notices must be preserved. Contributors provide an express grant of patent 
+rights.
+
+Made by JiJae (ruestgeo)
+*/
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
 const token = require('./auth.json').token;
-
-const prefix = "..jijaebot ";
+const configs = require('./configs.json');
+const prefix = configs.prefix;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
